8c41592b9b37132bc8c63fe827f98c5cae9c9c1b
Robin Ury <r.ury@tuta.io>
2017-05-02 13:59:19 -0500

    Game is working. Will refine and add game over, maybe options of play

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ Vue.component('gamebutton', {
   methods: {
     press: function () {
       console.log(this.color + ' pressed')
-      this.$emit(this.color)
+      Simon.listen(this.color)
     }
   }
 })
@@ -40,9 +40,8 @@ var Simon = new Vue({
   methods: {
     startGame: function () {
       Simon.addMove()
+      this.status = 'telling'
       Simon.tellMoves()
-      this.status = 'waiting'
-      console.log(this.status)
       // tell move 
       // wait -> player push gamebutt
       // if hear correct event -> goto 1
@@ -54,20 +53,24 @@ var Simon = new Vue({
     tellMoves: function () {
       var tell = setInterval(function () {
         var color = this.sequence[this.step]
-        $('#' + color).velocity({opacity: 0.72}, {duration: 500})
+        $('#' + color).velocity({opacity: 0.72}, {duration: 500 - this.round * 25})
         $('#' + color).velocity('reverse')
         if (this.step === this.sequence.length - 1) {
-          console.log('clearing')
+          this.status = 'waiting'
           clearInterval(tell)
           this.step = 0
         } else { this.step += 1 }
-      }.bind(Simon), 1500)
+      }.bind(Simon), 1500 - this.round * 25)
     },
     listen: function (color) {
       if (this.status !== 'waiting') { return }
-      if (color === sequence[step]) {
-        console.log('Correct')
-      }
+      if (color === this.sequence[this.step]) {
+        if (this.step === this.sequence.length - 1) {
+          this.step = 0
+          this.round += 1
+          Simon.startGame()
+        } else { this.step += 1 }
+      } else { this.status = 'gameOver'}
     }
   }
 })
